cc27e8a11bcfb76c5efeeb29bb64fcb8c3421214
tevea <teveachhem@gmail.com>
2026-02-23 14:10:24 +0700

    feat update from react.js to next.js

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yCUaHyqk6YAnCClc+A+8kkNZS+VyWMe43DaM5Hoq/P4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0e3J0x5CvhO0gq49WSl7sD/C3aSYDDjJI/cLWGimgAQ=\"\n}"
